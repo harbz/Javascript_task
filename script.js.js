cdf23bdfd37907ac1f99c6e37b1cf7fcc44b1850
harbz <54363762+harbz@users.noreply.github.com>
2020-04-11 12:29:11 +0100

    Add files via upload

---
+++ script.js.js
@@ -0,0 +1,19 @@
+let name = "Abayomi";
+let courses = "frontend, design";
+
+ console.log("My name is: " + name);
+ console.log("courses offered are: " + courses);
+
+ const data = [];
+
+ for (let i=0; i<=200; i++) {
+
+   if (i%2===0) {
+
+     data.push(i);
+
+   }
+
+ }
+
+ console.log(data);
